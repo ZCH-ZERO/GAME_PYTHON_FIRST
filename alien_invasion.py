3b8872d84b711a5209c48bc67c170f756e1afee5
ZXC-ZERO <15893851179@163.com>
2017-10-19 11:40:28 +0800

    alien_row
    创建一行外星人

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
+from alien import Alien
 
 
 def run_game():
@@ -15,10 +16,13 @@ def run_game():
 	screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
 
-	# 创建一艘飞船
+	# 创建一艘飞船, 一个子弹编组和一个外星人编组
 	ship = Ship(ai_settings, screen)
-	# 创建一个用于存储子弹的编组
 	bullets = Group()
+	aliens = Group()
+
+	# 创建外星人群
+	gf.create_fleet(ai_settings, screen, aliens)
 
 	# 设置背景色
 	# bg_color = (230,230,230)
@@ -32,7 +36,7 @@ def run_game():
 		ship.update()
 		gf.update_bullets(bullets)
 
-		gf.update_screen(ai_settings, screen, ship, bullets)
+		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 		# 每次循环时都重绘屏幕
 		screen.fill(ai_settings.bg_color)
